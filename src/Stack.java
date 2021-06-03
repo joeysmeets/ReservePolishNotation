4ae0e6e38f9c29d9c376adfcfeea5abe304a8412
Joey Smeets <joeysmeetsss@gmail.com>
2021-06-03 12:52:37 +0200

    There was a small mistake when copying. It is now solved.

--- src/Stack.java
+++ src/Stack.java
@@ -1,19 +1,22 @@
-
 public interface Stack<E> {
 	
 	void push(E element);
 	
-	void pop();
+	E pop() throws StackUnderflowException;
 	
 	void clear();
 	
-	E peek();
+	E peek() throws StackUnderflowException;
 	
 	E get(int index);
 	
 	int search(E element);
 	
+	int size();
+	
 	boolean contains(E element);
 	
 	boolean empty();
+	
+	String toString();
 }
